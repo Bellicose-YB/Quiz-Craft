d949d4459171393de1992bcba13050e5dbfb95ec
Bellicose-YB <60650007+Bellicose-YB@users.noreply.github.com>
2021-07-08 13:36:42 +0530

    auth done

--- Quiz_Craft/authentication/urls.py
+++ Quiz_Craft/authentication/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path, include
-
+from .views import SignUp,SignIn,PasswordReset,EmailConfirmation,Done,ConfirmPassword
 urlpatterns = [
     # path('', HomeView, name='home')
+    path('signup', SignUp, name='signup'),
+    path('signin', SignIn, name='signin'),
+    path('passwordreset', PasswordReset, name='passwordreset'),
+    path('emailconfirmation', EmailConfirmation, name='emailconfirmation'),
+    path('done', Done, name='done'),
+    path('confirmpassword', ConfirmPassword, name='confirmpassword'),
 ] 
--- Quiz_Craft/authentication/views.py
+++ Quiz_Craft/authentication/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def SignUp(request):
+    return render(request, 'SignUp.html')
+def SignIn(request):
+    return render(request, 'SignIn.html')
+def PasswordReset(request):
+    return render(request, 'PasswordReset.html')
+def EmailConfirmation(request):
+    return render(request, 'EmailConfirmation.html')
+def Done(request):
+    return render(request, 'done.html')
+def ConfirmPassword(request):
+    return render(request, 'ConfirmPassword.html')
