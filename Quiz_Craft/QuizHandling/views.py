7c6c9be8eff57d2d6f9ad1d09b5100267cdbe29f
Bellicose-YB <60650007+Bellicose-YB@users.noreply.github.com>
2021-07-12 15:21:28 +0530

    Quiz Creation App done

--- Quiz_Craft/QuizHandling/views.py
+++ Quiz_Craft/QuizHandling/views.py
@@ -1,7 +1,8 @@
+from django.db.models.query_utils import Q
 from django.shortcuts import redirect, render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 
-from .models import Quiz
+from .models import Questions, Quiz
 from .forms import QuizStarterForm,QuestionCreationForm
 
 def Home(request):
@@ -16,9 +17,16 @@ def Home(request):
         form = QuizStarterForm()
     return render(request, 'home.html', {'form': form})
 
+
+
 @login_required
 def CreateQuestionsView(request, pk):
     quiz = Quiz.objects.get(pk=pk)
+    Question = []
+    for q in quiz.getQuestions():
+        Question.append(q)
+    # print(Question)
+    print("yath")
     if request.method == "POST":
         form = QuestionCreationForm(request.POST)
         if form.is_valid():
@@ -26,11 +34,13 @@ def CreateQuestionsView(request, pk):
             ques.AuthorKey = request.user
             ques.QuizKey = quiz
             ques.save()
+            print("yatharth")
             return redirect('createquestions', pk=quiz.id)
+            # return render(request, 'make_questions.html', {'form': form, 'Question' : Question, 'pk':pk})
     else:
         form = QuestionCreationForm()
     # print(quiz)
     #@TODO CHECK HERE IF CURRENT QUIZ AUTHOR IS SAME AS OUR LOGIN USER
-
+    print("ystha")
     #@TODO MAKE A FORM FOR QUESTIONS AND VALIDATE IT
-    return render(request, 'make_questions.html' ,{'form': form, 'pk' : pk})
+    return render(request, 'make_questions.html' ,{'form': form, 'pk' : pk,'Question' : Question})
