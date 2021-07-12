30f36c9849a928ee0b92aba087ca9f147b944094
Yash jain <jainism987e@gmail.com>
2021-07-12 12:53:36 +0530

    connect forms to views

--- Quiz_Craft/QuizHandling/views.py
+++ Quiz_Craft/QuizHandling/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+
+from .models import Quiz
+from .forms import QuizStarterForm
 
 def Home(request):
-    return render(request, 'home.html')
+    if request.method == "POST":
+        form = QuizStarterForm(request.POST)
+        if form.is_valid():
+            note = form.save(commit=False)
+            note.Author = request.user
+            note.save()
+            return redirect('createquestions', pk=note.id)
+    else:
+        form = QuizStarterForm()
+    return render(request, 'home.html', {'form': form})
+
+@login_required
+def CreateQuestionsView(request, pk):
+    quiz = Quiz.objects.get(pk=pk)
+    print(quiz)
+    #@TODO CHECK HERE IF CURRENT QUIZ AUTHOR IS SAME AS OUR LOGIN USER
+
+    #@TODO MAKE A FORM FOR QUESTIONS AND VALIDATE IT
+    return render(request, 'make_questions.html')
